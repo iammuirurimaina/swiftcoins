68ada5288ec8ac4a0ed45d20344d0e5a2dbcd508
annitakaranja <annitakaranja27@gmail.com>
2024-07-12 11:39:08 +0300

    Update Statements file

--- swiftcoinsapp/lib/Statement.dart
+++ swiftcoinsapp/lib/Statement.dart
@@ -6,7 +6,7 @@ import 'custom_scaffold.dart';
 const List<String> list = <String>['Withdraw', 'Deposits', 'Loans'];
 
 class Statement extends StatelessWidget {
-  const Statement({Key? key}) : super(key: key);
+  const Statement({super.key});
   final account = 111111111;
   final balance = 1000;
 
@@ -14,10 +14,10 @@ class Statement extends StatelessWidget {
   Widget build(BuildContext context) {
     return
 
-        /// `CustomScaffold` is a custom widget that is being used to create a scaffold with a
+        /// CustomScaffold is a custom widget that is being used to create a scaffold with a
         /// specific layout for the Withdraw screen. It likely includes features such as a custom app
         /// bar, body content, and possibly other customizations specific to this screen.
-        /// Unfortunately, the implementation of `CustomScaffold` is not provided in the code
+        /// Unfortunately, the implementation of CustomScaffold is not provided in the code
         /// snippet, so we can't see its exact functionality.
         CustomScaffold(
       title: 'Mini-Statement',
@@ -27,19 +27,6 @@ class Statement extends StatelessWidget {
         children: [
           Row(mainAxisAlignment: MainAxisAlignment.end, children: [
             DropdownMenu(),
-            SizedBox(height: 20),
-            DownloadButton(
-              status: DownloadStatus.notDownloaded,
-              onDownload: () {
-                // Handle download logic
-              },
-              onCancel: () {
-                // Handle cancel logic
-              },
-              onOpen: () {
-                // Handle open logic
-              },
-            ),
           ]),
           Center(child: DataTableExample()),
         ],
@@ -78,151 +65,9 @@ class _DropdownMenuState extends State<DropdownMenu> {
   }
 }
 
-enum DownloadStatus {
-  notDownloaded,
-  fetchingDownload,
-  downloading,
-  downloaded,
-}
 
-class DownloadButton extends StatelessWidget {
-  DownloadButton({
-    super.key,
-    required this.status,
-    this.downloadProgress = 0,
-    required this.onDownload,
-    required this.onCancel,
-    required this.onOpen,
-    this.transitionDuration = const Duration(milliseconds: 500),
-  });
 
-  final DownloadStatus status;
-  final double downloadProgress;
-  final VoidCallback onDownload;
-  final VoidCallback onCancel;
-  final VoidCallback onOpen;
-  final Duration transitionDuration;
 
-  bool get _notDownloaded => status == DownloadStatus.notDownloaded;
-
-  bool get _isDownloading => status == DownloadStatus.downloading;
-
-  bool get _isFetching => status == DownloadStatus.fetchingDownload;
-
-  bool get _isDownloaded => status == DownloadStatus.downloaded;
-
-  void _onPressed() {
-    switch (status) {
-      case DownloadStatus.notDownloaded:
-        onDownload();
-      case DownloadStatus.fetchingDownload:
-        // do nothing.
-        break;
-      case DownloadStatus.downloading:
-        onCancel();
-      case DownloadStatus.downloaded:
-        onOpen();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _onPressed,
-      child: Stack(
-        children: [
-          ButtonShapeWidget(
-            transitionDuration: transitionDuration,
-            isDownloaded: _isDownloaded,
-            isDownloading: _isDownloading,
-            isFetching: _isFetching,
-          ),
-          Positioned.fill(
-            child: AnimatedOpacity(
-              duration: transitionDuration,
-              opacity: _isDownloading || _isFetching ? 1.0 : 0.0,
-              curve: Curves.ease,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  ProgressIndicatorWidget(
-                    downloadProgress: downloadProgress,
-                    isDownloading: _isDownloading,
-                    isFetching: _isFetching,
-                  ),
-                  if (_isDownloading)
-                    const Icon(
-                      Icons.stop,
-                      size: 14.0,
-                      color: CupertinoColors.activeBlue,
-                    ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-ProgressIndicatorWidget(
-    {required double downloadProgress,
-    required bool isDownloading,
-    required bool isFetching}) {}
-
-class ButtonShapeWidget extends StatelessWidget {
-  const ButtonShapeWidget({
-    super.key,
-    required this.isDownloading,
-    required this.isDownloaded,
-    required this.isFetching,
-    required this.transitionDuration,
-  });
-
-  final bool isDownloading;
-  final bool isDownloaded;
-  final bool isFetching;
-  final Duration transitionDuration;
-
-  @override
-  Widget build(BuildContext context) {
-    var shape = const ShapeDecoration(
-      shape: StadiumBorder(),
-      color: CupertinoColors.lightBackgroundGray,
-    );
-
-    if (isDownloading || isFetching) {
-      shape = ShapeDecoration(
-        shape: const CircleBorder(),
-        color: Colors.white.withOpacity(0),
-      );
-    }
-
-    return AnimatedContainer(
-      duration: transitionDuration,
-      curve: Curves.ease,
-      width: double.infinity,
-      decoration: shape,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 6),
-        child: AnimatedOpacity(
-          duration: transitionDuration,
-          opacity: isDownloading || isFetching ? 0.0 : 1.0,
-          curve: Curves.ease,
-          child: Text(
-            isDownloaded ? 'OPEN' : 'DOWNLOAD',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: CupertinoColors.activeOrange,
-                ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 class DataTableExample extends StatelessWidget {
   const DataTableExample({super.key});
